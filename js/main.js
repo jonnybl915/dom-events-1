6b2807a3f64fcb1ed0e6996ca581e3963d194753
jonathandavidblack <jonnybl915@gmail.com>
2016-10-11 17:58:06 -0400

    completed task 2, need to go over currentTarget info

--- js/main.js
+++ js/main.js
@@ -2,7 +2,49 @@
 
 // TASK 1 -- Show/Hide Nav
 
+//selecting the element to which to add the click event
+var button = document.querySelector('.hideOrShow');
+//setting up the callback function
+var hideOrShowNav = function() {
+    // console.log(button.innerHTML);
+    if (button.innerHTML === "Hide Nav") {
+        button.innerHTML = "Show Nav";
+    } else {
+        button.innerHTML = "Hide Nav";
+    }
+    var navMenu = document.querySelector('.nav-menu');
+    console.log([navMenu]);
+    var navMenuStyles = window.getComputedStyle(navMenu);
+    //console.log(navMenuStyles);
+    console.log('---------------------------------');
+
+    console.log([navMenuStyles]);
+
+    if (navMenuStyles.visibility === "visible") {
+        navMenu.style.visibility = "hidden";
+    } else {
+        navMenu.style.visibility = "visible";
+    }
+}
+console.log('-------------------------');
+
+button.addEventListener('click', hideOrShowNav);
+
+
 // TASK 2 -- Select an Icon
+
+var selectOrDeselect = function(iconList){
+  if (iconList.currentTarget.classList.contains('selected')) {
+    iconList.currentTarget.classList.remove('selected');
+  } else {
+    iconList.currentTarget.classList.add('selected')
+  }
+}
+
+var iconList = document.querySelectorAll('.option');
+for (var i = 0; i < iconList.length; i++) {
+  iconList[i].addEventListener('click', selectOrDeselect)
+}
 
 // TASK 3 -- Move Item From List to List
 
